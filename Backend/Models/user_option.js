94efc07b8f63060811cd9e8bf95142533538cb56
unknownmeowmeow <164880015+unknownmeowmeow@users.noreply.github.com>
2025-10-29 11:52:37 +0800

    done

--- Backend/Models/user_option.js
+++ Backend/Models/user_option.js
@@ -7,13 +7,14 @@ class UserPollOptions{
 
     /**
      * Inserts multiple poll options for a specific question.
-     * @param {Array} option_data - Array of objects containing question_id and option name.
+     * @param {Array} poll_option_data - Array of objects containing question_id and option name.
+     * @param {Array} poll_option_values - Array of objects containing question_id and option name.
      * @param {Object} connection - Database connection (default: this.db).
      * @returns {Object} response_data - Object containing status, result with affected rows, or error.
      * Last Updated At: October 28, 2025
      * @author Keith
      */
-    async insertPollOptions(poll_option_data,poll_option_values, connection = this.db){
+    async insertPollOptions(poll_option_data, poll_option_values, connection = this.db){
         const response_data = { status: false, result: null, error: null };
     
         try{
